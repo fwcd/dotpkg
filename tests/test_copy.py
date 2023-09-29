5aa9e526c1358a249c47046745dc7bd34cf9f490
fwcd <fwcdmail@gmail.com>
2023-09-29 16:21:42 +0100

    Add copy package for testing

---
+++ tests/test_copy.py
@@ -0,0 +1,16 @@
+import unittest
+
+from tests.fixtures import SourcePkgFixture, HomeDirFixture
+
+class TestCopy(unittest.TestCase):
+    def test_copy(self):
+        pkg = SourcePkgFixture('copy')
+
+        with HomeDirFixture() as home:
+            with pkg.install_context(home.opts):
+                self.assertFalse((home.path / 'dir').is_symlink())
+                self.assertTrue((home.path / 'dir').is_dir())
+                self.assertTrue((home.path / 'dir' / 'a.txt').is_file())
+            self.assertTrue(home.is_empty)
+
+            # TODO: Test failing uninstallations (e.g. if hashes mismatch)
