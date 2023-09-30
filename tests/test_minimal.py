46d3a1c1cb1adae0c7733f9d9fc7e320371a1e89
fwcd <fwcdmail@gmail.com>
2023-09-30 21:58:41 +0100

    Test minimal package with and without install manifest

--- tests/test_minimal.py
+++ tests/test_minimal.py
@@ -1,14 +1,27 @@
 import unittest
 
+from dataclasses import replace
 from tests.fixtures import DotpkgFixture, HomeDirFixture
 
 class TestMinimal(unittest.TestCase):
-    def test_minimal(self):
-        pkg = DotpkgFixture('minimal')
+    def setUp(self) -> None:
+        self.pkg = DotpkgFixture('minimal')
 
+    def test_with_install_manifest(self):
         with HomeDirFixture() as home:
-            with pkg.install_context(home.opts):
+            opts = replace(home.opts, update_install_manifest=True)
+
+            with self.pkg.install_context(opts):
                 self.assertTrue((home.path / 'hello.txt').is_symlink())
 
             self.assertFalse((home.path / 'hello.txt').exists())
             self.assertEqual(home.read_install_manifest().installs, {})
+
+    def test_without_install_manifest(self):
+        with HomeDirFixture() as home:
+            opts = replace(home.opts, update_install_manifest=False)
+
+            with self.pkg.install_context(opts):
+                self.assertTrue((home.path / 'hello.txt').is_symlink())
+
+            self.assertTrue(home.is_empty)
