1795ca12c61f90ad68b76ba08c1b0d5eac3e26b7
fwcd <fwcdmail@gmail.com>
2023-10-04 23:28:40 +0100

    Add todo notes

--- dotpkg/options.py
+++ dotpkg/options.py
@@ -5,10 +5,11 @@ from pathlib import Path
 class Options:
     cwd: Path = Path.cwd()
     home: Path = Path.home()
-    dry_run: bool = False
-    assume_yes: bool = False
     safe_mode: bool = False
     update_install_manifest: bool = True
+
+    dry_run: bool = False # TODO: Replace with a 'file system' interface
+    assume_yes: bool = False # TODO: Replace with a 'decider' interface
 
     @property
     def state_dir(self) -> Path:
