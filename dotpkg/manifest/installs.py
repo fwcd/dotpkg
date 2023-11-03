4f35b4670f221e87c1272fa469c51271019d3c15
fwcd <fwcdmail@gmail.com>
2023-11-03 13:13:36 +0000

    Fix installs schema

--- dotpkg/manifest/installs.py
+++ dotpkg/manifest/installs.py
@@ -4,7 +4,8 @@
 from .installs_v1 import InstallsV1Manifest
 from .installs_v2 import InstallsV2Manifest
 from .installs_v3 import InstallsV3Manifest
+from .installs_v4 import InstallsV4Manifest
 from typing import Union
 
-InstallsManifest = Union[InstallsV1Manifest, InstallsV2Manifest, InstallsV3Manifest]
+InstallsManifest = Union[InstallsV1Manifest, InstallsV2Manifest, InstallsV3Manifest, InstallsV4Manifest]
 
